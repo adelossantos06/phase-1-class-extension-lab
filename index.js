98127c397fb563c1a967be2d0b7861ed3835cabf
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-08-11 09:18:29 -0500

    completes class extension lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+class Polygon {
+    constructor(intgArray) {
+        this.intgArray = intgArray
+    }
+
+    get countSides() {
+        return this.intgArray.length
+    }
+
+    get perimeter() {
+        let sum = 0;
+        this.intgArray.forEach(item => {
+            sum += item
+        })
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+
+        let a = this.intgArray[0]
+        let b = this.intgArray[1]
+        let c = this.intgArray[2]
+
+        if ((a + b > c) && (a + c > b) && (c + b > a)) {
+            return true
+        }
+        return false
+    }
+}
+
+
+class Square extends Polygon {
+
+    get isValid() {
+        const isSquare = this.intgArray.every(i => i === this.intgArray[0])
+
+        if (isSquare === true) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        if (this.isValid === true) {
+            return (this.intgArray[0] * this.intgArray[1])
+        }
+    }
+
+}
